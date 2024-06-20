a78c1153a6a6b9dc3feace2d5976352e923a6be9
simone menichetti <simon3menichettui95@gmail.com>
2024-06-20 15:47:33 +0200

    build:(initialize milestorm 3 for eventlistenr 'click)

--- js/main.js
+++ js/main.js
@@ -68,36 +68,36 @@ function generatePostlayout(posts) {
 
 
     // ciclo for each per costruire il markup cell'html per ciascuno dei post , utilizzando template viene aggiunto alla stringa html del dom.
-    posts.forEach(post => {
+    posts.forEach(element => {
 
         // verifica gli oggetti post generati nel ciclo dei posts
         // console.log(post);
-        
+
         html += `
             <div class="post">
                 <div class="post__header">
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">
+                        <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author">${post.author.name}</div>
-                        <div class="post-meta__time">${post.created}</div>
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
                     </div>
                 </div>
-                <div class="post__text">${post.content}</div>
+                <div class="post__text">${element.content}</div>
                 <div class="post__image">
-                    <img src="${post.media}" alt="">
+                    <img src="${element.media}" alt="">
                 </div>
                 <div class="post__footer">
                     <div class="likes js-likes">
                         <div class="likes__cta">
-                            <a class="like-button js-like-button" href="#" data-postid="${post.id}">
+                            <a class="like-button js-like-button" href="#" data-postid="${element.id}">
                                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                                 <span class="like-button__label">Mi Piace</span>
                             </a>
                         </div>
                         <div class="likes__counter">
-                            Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone
+                            Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
                         </div>
                     </div>
                 </div>
